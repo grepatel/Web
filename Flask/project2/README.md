0d4c315984c0224e46aa7759304c84af614543dc
gpatel7 <63757577+gpatel7@users.noreply.github.com>
2020-04-22 17:01:29 -0400

    Chatroom App

---
+++ Flask/project2/README.md
@@ -0,0 +1,3 @@
+p# Project 2
+
+Web Programming with Python and JavaScript
---
+++ Flask/project2/static/script.js
@@ -0,0 +1,13 @@
+if(!localStorage.getItem('chatUserName'))
+    localStorage.setItem('chatUserName','');
+    document.addEventListener('DOMContentLoaded',() =>
+    {
+
+        document.querySelector('button').onclick = () =>
+        {
+
+            let chatName = document.getElementById('chatUserName').value;
+            localStorage.setItem('chatUserName',chatName);
+            document.querySelector('#msg').innerHTML = 'Hello ' + chatName + '!!';
+        }
+    })
