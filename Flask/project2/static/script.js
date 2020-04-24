ed69821afdc9eb4c24291c44140a37af9f701dc5
gpatel7 <63757577+gpatel7@users.noreply.github.com>
2020-04-24 00:34:22 -0400

    added chatroom basics. Server side snapshot pending

--- Flask/project2/static/script.js
+++ Flask/project2/static/script.js
@@ -1,22 +1,48 @@
 
 document.addEventListener('DOMContentLoaded',() =>
 {
-        if(!localStorage.getItem('chatUserName'))
+        if(!localStorage.getItem('username'))
         {
-            localStorage.setItem('chatUserName','');
+            localStorage.setItem('username','');
             document.querySelector('#frmChatName').style.display='block';
+            document.querySelector('#btnChangeChatName').style.display='none';
         }
         else
         {
+
+            document.querySelector('#msg').innerHTML = "Hello " + localStorage.getItem('username') + "!!";
             document.querySelector('#frmChatName').style.display='none';
+            document.querySelector('#btnChangeChatName').style.display='block';
         }
 
-        document.querySelector('button').onclick = () =>
+        if(!localStorage.getItem('chatroomname'))
+        {
+            const chatroomName='Default';
+            localStorage.setItem('chatroomname',chatroomName);
+            AddListItem('#lstChatroom',chatroomName);
+        }
+
+        document.querySelector('#btnChatName').onclick = () =>
         {
 
             let chatName = document.getElementById('chatUserName').value;
-            localStorage.setItem('chatUserName',chatName);
-            document.querySelector('#msg').innerHTML = 'Hello ' + chatName + '!!';
+            localStorage.setItem('username',chatName);
+            document.querySelector('#msg').innerHTML = "Hello " + chatName + "!!";
+            document.querySelector('#frmChatName').style.display='none';
+            document.querySelector('#btnChangeChatName').style.display='block';
+
+        }
+
+        document.querySelector('#btnChangeChatName').onclick = () =>
+        {
+            document.querySelector('#frmChatName').style.display='block';
+            document.querySelector('#btnChangeChatName').style.display='none';
+        }
+
+        document.querySelector("#btnCreateChatroom").onclick = () =>
+        {
+            chatroomName=document.querySelector('#txtChatroomName').value;
+            AddListItem('#lstChatroom',chatroomName);
         }
 
     var socket = io();
@@ -25,7 +51,7 @@ document.addEventListener('DOMContentLoaded',() =>
 
         document.querySelector('#btnSendMsg').onclick = () =>
         {
-                const msg = localStorage.getItem('chatUserName') + ": " + document.querySelector('#txtMsg').value + '\n';
+                const msg = localStorage.getItem('username') + ": " + document.querySelector('#txtMsg').value + '\n';
                 socket.emit('send message',{'message':msg});
                 document.querySelector('#txtMsg').value='';
         };
@@ -36,3 +62,13 @@ document.addEventListener('DOMContentLoaded',() =>
           document.querySelector('#txtChatWindow').value=data.chatText;
     });
 });
+
+function AddListItem(ulId,liText)
+{
+                var ul = document.querySelector(ulId);
+                 var li = document.createElement('li');
+                li.appendChild(document.createTextNode(liText));
+                li.setAttribute('id', liText);
+                li.setAttribute('class','list-group-item')
+                ul.appendChild(li);
+}
