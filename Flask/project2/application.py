9d92763a1c537ad20d8626063aea8c92969fecfa
gpatel7 <63757577+gpatel7@users.noreply.github.com>
2020-04-25 01:14:02 -0400

    added chatrooms... browser snapshot of chatrooms pending basic validations pending

--- Flask/project2/application.py
+++ Flask/project2/application.py
@@ -1,26 +1,109 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-chatText = ""
+
+class Chatroom:
+    users = []
+
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
+
+    def add(self, name):
+        self.name = name
+
+    def newmessage(self, msg):
+        self.text += msg
+
+    def adduser(self, username):
+        self.users.append(username)
+
+    def removeuser(self, username):
+        self.users.remove(username)
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(**data)
+
+    def serialize(self):
+        data = json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        return data
+
+
+chatrooms = [Chatroom("Default", "line0")]
+
+
+def Serialize(chatrooms):
+    data = json.dumps(chatrooms, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    return data
+
+
+def Deserialize(data):
+    global chatrooms
+    chatrooms = Chatroom.from_json(json.loads(data))
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", chatText=chatText)
+    chatroomsJson = Serialize(chatrooms)
+    print('root Route : ' + chatroomsJson)
+    return render_template("index.html", chatroomsJson=chatroomsJson)
+
+
+@app.route("/<string:name>")
+def room(name):
+    chatroom = next(filter(lambda x: x.name == name, chatrooms))
+    if chatroom is not None:
+        print('found chatroom ' + json.jsonify(chatroom))
+        return json.jsonify(chatroom);
 
 
 @socketio.on("send message")
 def sendmessage(data):
-    global chatText
-    print('received json: ' + data["message"])
-    chatText += data["message"]
-    emit("broadcast", {"chatText": chatText}, broadcast=True)
+    print('received message: ' + data["chatroom"])
+    name = data["chatroom"]
+    msg = data["message"]
+    chatroom= next((x for x in chatrooms if x.name == name), None)
+    if chatroom is not None:
+        print('received message: found chatroom ' + data["message"])
+        chatroom.newmessage(msg)
+        emit("broadcast msg", {"text": chatroom.text}, broadcast=True)
+
+
+@socketio.on("new chatroom")
+def on_newchatroom(data):
+    name = data["chatroom"]
+    user = data["username"]
+    chatroom = Chatroom(name, "")
+    chatroom.adduser(user)
+    chatrooms.append(chatroom)
+    emit("broadcast chatroom", chatroom.serialize(), broadcast=True)
+
+
+@socketio.on('join')
+def on_join(data):
+    name = data["room"]
+    user = data["username"]
+    chatroom = next(filter(lambda x: x.name == name, chatrooms))
+    if chatroom is not None:
+        chatroom.adduser(user)
+        send(user + ' has entered the room.', chatrooms=Serialize(chatrooms))
+
+
+@socketio.on('leave')
+def on_leave(data):
+    name = data["room"]
+    user = data["username"]
+    chatroom = next(filter(lambda x: x.name == name, chatrooms))
+    if chatroom is not None:
+        chatroom.removeuser(user)
+        send(user + ' has exited the room.', chatrooms=Serialize(chatrooms))
 
 
 @socketio.on('connect')
@@ -36,22 +119,6 @@ def test_disconnect():
 @socketio.on('json')
 def handle_json(json):
     print('received json: ' + str(json))
-
-
-@socketio.on('join')
-def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
-
-
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
 
 
 @socketio.on("my error event")
--- Flask/project2/static/script.js
+++ Flask/project2/static/script.js
@@ -1,6 +1,60 @@
 
 document.addEventListener('DOMContentLoaded',() =>
 {
+        Load_Page();
+        document.querySelector('#btnChatName').onclick = () =>
+        {
+
+            let chatName = document.getElementById('chatUserName').value;
+            localStorage.setItem('username',chatName);
+            document.querySelector('#msg').innerHTML = "Hello " + chatName + "!!";
+            document.querySelector('#frmChatName').style.display='none';
+            document.querySelector('#btnChangeChatName').style.display='block';
+
+        }
+
+        document.querySelector('#btnChangeChatName').onclick = () =>
+        {
+            document.querySelector('#frmChatName').style.display='block';
+            document.querySelector('#btnChangeChatName').style.display='none';
+        }
+
+        document.querySelector("#btnCreateChatroom").onclick = () =>
+        {
+            const chatroomName=document.querySelector('#txtChatroomName').value;
+            socket.emit('new chatroom',{'chatroom':chatroomName,'username':localStorage.getItem('username')});
+            localStorage.setItem('chatroomname',chatroomName);
+        }
+
+    var socket = io();
+    socket.on('connect', function()
+    {
+
+        document.querySelector('#btnSendMsg').onclick = () =>
+        {
+                const chatroomName=localStorage.getItem('chatroomname');
+                const msg = localStorage.getItem('username') + ": " + document.querySelector('#txtMsg').value + '\n';
+                socket.emit('send message',{'chatroom':chatroomName, 'message':msg});
+                document.querySelector('#txtMsg').value='';
+        };
+    });
+
+    socket.on('broadcast msg',data =>
+    {
+          document.querySelector('#txtChatWindow').value=data.text;
+    });
+
+    socket.on('broadcast chatroom',data =>
+    {
+          console.log("caught broadcast chatroom event");
+          const chatroom = JSON.parse(data);
+          console.log("caught broadcast chatroom event :: " + chatroom.name)
+          AddListItem('#lstChatroom',chatroom.name);
+    });
+});
+
+function Load_Page()
+{
         if(!localStorage.getItem('username'))
         {
             localStorage.setItem('username','');
@@ -22,46 +76,15 @@ document.addEventListener('DOMContentLoaded',() =>
             AddListItem('#lstChatroom',chatroomName);
         }
 
-        document.querySelector('#btnChatName').onclick = () =>
-        {
+        var chatrooms  = JSON.parse(document.querySelector('#serverChatObj').dataset.chatrooms);
 
-            let chatName = document.getElementById('chatUserName').value;
-            localStorage.setItem('username',chatName);
-            document.querySelector('#msg').innerHTML = "Hello " + chatName + "!!";
-            document.querySelector('#frmChatName').style.display='none';
-            document.querySelector('#btnChangeChatName').style.display='block';
+        chatrooms.forEach(function(room) {
+            AddListItem('#lstChatroom',room.name);
+            })
 
-        }
-
-        document.querySelector('#btnChangeChatName').onclick = () =>
-        {
-            document.querySelector('#frmChatName').style.display='block';
-            document.querySelector('#btnChangeChatName').style.display='none';
-        }
-
-        document.querySelector("#btnCreateChatroom").onclick = () =>
-        {
-            chatroomName=document.querySelector('#txtChatroomName').value;
-            AddListItem('#lstChatroom',chatroomName);
-        }
-
-    var socket = io();
-    socket.on('connect', function()
-    {
-
-        document.querySelector('#btnSendMsg').onclick = () =>
-        {
-                const msg = localStorage.getItem('username') + ": " + document.querySelector('#txtMsg').value + '\n';
-                socket.emit('send message',{'message':msg});
-                document.querySelector('#txtMsg').value='';
-        };
-    });
-
-    socket.on('broadcast',data =>
-    {
-          document.querySelector('#txtChatWindow').value=data.chatText;
-    });
-});
+        const chatroom = chatrooms.find( x => x.name ===localStorage.getItem('chatroomname'));
+        document.querySelector('#txtChatWindow').value = chatroom?chatroom.text:"";
+}
 
 function AddListItem(ulId,liText)
 {
